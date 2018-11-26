c4443ab92f617bce3f0ef18b979322435dd413a0
yu-ig <archiodyssey@yahoo.com>
2018-11-26 14:22:30 +0900

    ..

--- app.py
+++ app.py
@@ -87,14 +87,14 @@ def handle_message(event):
         # print("::: " + str(data))
         # print(len(data), 'bytes; md:', metadata)
         # if watchword  dict
-        url = 'https://damp-sands-30274.herokuapp.com/'
+        url = 'https://damp-sands-30274.herokuapp.com/send/00.mp4'
         path = '00.mp4'
         line_bot_api.reply_message(
             event.reply_token,
             # TextSendMessage(text='SUCCESS!! ' )
 
             VideoSendMessage(
-                original_content_url=url+path
+                original_content_url=url
                 # preview_image_url='https://example.com/preview.jpg'
             )
             #TextSendMessage(text=event.message.text)
