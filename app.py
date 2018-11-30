aa2f5d34804eec87ce84e8217ea0d5e7db59e0b1
yu-ig <archiodyssey@yahoo.com>
2018-12-01 07:23:06 +0900

    ..

--- app.py
+++ app.py
@@ -147,7 +147,7 @@ def post(post_text):
         f2.write(str(json.dumps(json_data, indent=4)))
 
 
-    dbx.files_download_to_file(t[0], 'static/' + t[0])
+    # dbx.files_download_to_file(t[0], 'static/' + t[0])
 
 
     return str(t[0]+" "+t[1] + " " +t[2])
